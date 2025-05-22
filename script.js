ccd615b1731770552e80faf7a3bb5e1b4036860f
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-05-22 21:34:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+window.onload = function() {
+  // Get references to the dropdown and the button
+  const selectElement = document.getElementById("colorSelect");
+  const button = document.querySelector('input[type="button"]');
+
+  // Add a click event listener to the button
+  button.addEventListener("click", function() {
+    // Get the selected index from the dropdown
+    const selectedIndex = selectElement.selectedIndex;
+
+    // If a valid option is selected, remove it
+    if (selectedIndex !== -1) {
+      selectElement.remove(selectedIndex);
+    }
+  });
+};
